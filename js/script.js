bd00c8e114aa48436b036affc4737f4c2481962f
Arthur <example@gmail.com>
2023-02-01 19:23:48 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use script";
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log('srting' * 9 );
+
+const person = "Alex";
+
+const bool = false;
+
+// console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: 'Jhon',
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+let arr = ['plum.png', 'orange.jpg', 'apple,bmp', 6, {}, []];
+console.log(arr[0]);
